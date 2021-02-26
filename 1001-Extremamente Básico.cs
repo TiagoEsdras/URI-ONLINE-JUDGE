b0f3877435e815c3efd7652b365cfe59156ce679
Tiago Esdras <68123518+TiagoEsdras@users.noreply.github.com>
2021-02-26 12:14:54 -0300

    1001 - Extremamente Básico

---
+++ 1001-Extremamente Básico.cs
@@ -0,0 +1,12 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int A, B, X;
+        A = Convert.ToInt32(Console.ReadLine());
+        B = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("X = {0}", X = A + B);
+    }
+}
