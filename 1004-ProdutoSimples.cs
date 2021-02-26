a3889b8a09a9f9f03f09d7504aba861e93ba0418
Tiago Esdras <68123518+TiagoEsdras@users.noreply.github.com>
2021-02-26 12:21:21 -0300

    1004 - Produto Simples

---
+++ 1004-ProdutoSimples.cs
@@ -0,0 +1,15 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int valor1, valor2;
+
+        valor1 = Convert.ToInt32(Console.ReadLine());
+        valor2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("PROD = {0}", valor1 * valor2);
+            
+    }
+}
