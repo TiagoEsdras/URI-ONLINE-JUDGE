a853859bd0e2683139e37e3d3ae6b7a05f3c22b6
Tiago Esdras <68123518+TiagoEsdras@users.noreply.github.com>
2021-02-26 12:19:33 -0300

    1003 - Soma Simples

---
+++ 1003-SomaSimples.cs
@@ -0,0 +1,13 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int A, B;
+        A = Convert.ToInt32(Console.ReadLine());
+        B = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("SOMA = {0}", A + B);
+    }
+}
